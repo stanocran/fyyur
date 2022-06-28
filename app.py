3265e039924069ea042c1915efaa62cdd13251da
stanocran <stanocran@gmail.com>
2022-06-28 10:42:34 +0000

    optimized import structure

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ import json
 from unicodedata import name
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import (Flask, 
+                   render_template, 
+                   request, 
+                   redirect, 
+                   Response,
+                   jsonify, 
+                   url_for, 
+                   flash)
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
